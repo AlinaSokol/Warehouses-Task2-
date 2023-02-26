8a45b52e44b57c0dba8ee6a31b9166f82320de5e
Any <aliyxy.3.5@gmail.com>
2023-02-26 20:22:02 +0300

    Init commit

---
+++ back.php
@@ -0,0 +1,87 @@
+<?php 
+$address = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'warehouses';
+$charset = 'utf8';
+
+try {
+	$pdo = new PDO ("mysql:host=$address;dbname=$database;cahrset=$charset", $username, $password);
+	$pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "Exception: " . $e->getMessage();
+} 
+
+$file = file_get_contents('https://bfdev.ru/test/json.txt');
+$data = json_decode($file, true);
+
+foreach ($data["NA_SKLADE"] as $key => $value) {
+	$warehouse1_quantity[] = $value[1]["QUANTITY"];
+	$warehouse2_quantity[] = $value[0]["QUANTITY"];
+	$goods[] = $key;
+}
+
+$insertDataFromFile = function() use($goods, $warehouse1_quantity, $warehouse2_quantity){
+	
+	for ($i=0; $i < count($goods); $i++) { 
+		$sql_insert_data = "INSERT INTO `remnants of goods`(goods, warehouse1_quantity, warehouse2_quantity) VALUES(?, ?, ?)";
+		$insert_data = $pdo->prepare($sql_insert_data);
+		$insert_data->execute([$goods[$i], $warehouse1_quantity[$i], $warehouse2_quantity[$i]]);
+	}
+};
+//$insertDataFromFile();
+
+$sumQuantityWh = function ($wh) use ($pdo) {
+	$sql_sum_quantity_wh = "SELECT warehouse" . "$wh" . "_quantity FROM `remnants of goods` WHERE warehouse" . "$wh" . "_quantity > 0";
+	$rst_sum_quantity_wh = $pdo->query($sql_sum_quantity_wh);
+	$sum_quantity_wh = $rst_sum_quantity_wh->fetchAll(PDO::FETCH_COLUMN, 0);
+	//print_r($sum_quantity_wh);
+	/*for ($i=0; $i < count($sum_quantity_wh); $i++) { 
+		$array[] = $sum_quantity_wh[$i]["warehouse" . "$wh" . "_quantity"];
+	}*/
+	$sum_array = array_sum($sum_quantity_wh);
+
+	return $sum_array;
+};
+
+$shortCodeNomenclature = function($code) { 
+	preg_match("/\w+-\w+/", $code, $short);
+	$short = reset($short); 
+
+	return $short;
+};
+
+$shortNomenclaturesWithItems = function($goods, $warehouse_quantity) use ($shortCodeNomenclature) {
+	$result = [];
+	for ($i = 0; $i < count($goods); $i++) { 
+		$shortNames[] = $shortCodeNomenclature($goods[$i]);
+
+		if ($warehouse_quantity[$i] > 0) {
+			$result[$i] = [
+				'short_names' => $shortNames[$i],
+				'quantity' => $warehouse_quantity[$i]
+			];
+		}
+	}	 
+
+	return $result;
+};
+
+$convertToJson = function($goods, $shortNamesList) {
+	$jsonArray = [];
+	
+	foreach($shortNamesList as $i => $shortNames) {
+
+		foreach($shortNames as $key => $item) {
+			$fullName = $goods[$key]; 
+			$jsonArray[$fullName] = [$item['short_names'], "склад " . ++$i, $item['quantity']];
+		}
+	}
+	//echo '<pre>';
+	//var_dump($jsonArray);
+	//echo '</pre>';
+	return json_encode($jsonArray);
+};
+
+
+//$data = json_encode($jsonArray);
---
+++ front.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Warehouses</title>
+	<style>
+		div {
+			display: inline-block;
+			width: 40%;
+			vertical-align: top;
+			padding-left:100px ;
+		}
+	</style>
+</head>
+<body>
+	<?php 
+		include "back.php";
+
+		$address = 'localhost';
+		$username = 'root';
+		$password = '';
+		$database = 'warehouses';
+		$charset = 'utf8';
+		try {
+			$pdo = new PDO ("mysql:host=$address;dbname=$database", $username, $password);
+			$pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			echo "Exception: " . $e->getMessage();
+		} 
+	?>
+	
+	<div>
+		<label>Остатки склада 1</label>
+		<ul>
+			<?php
+				$shortNomenclatures1  = $shortNomenclaturesWithItems($goods, $warehouse1_quantity);
+				foreach ($shortNomenclatures1 as $key => $value) {
+						echo '<li>Код товара: ' . $value['short_names'] . ', Остаток: ' . $value['quantity'] . 'шт;</li>';
+
+				} 
+			?>
+		</ul>
+		<p>Итого для первого склада: <?php echo $sumQuantityWh(1);?> шт</p>
+	</div>
+	<div>
+		<label>Остатки склада 2</label>
+		<ul>
+			<?php
+				$shortNomenclatures2 = $shortNomenclaturesWithItems($goods, $warehouse2_quantity);
+				foreach ($shortNomenclatures2 as $key => $value) {
+						echo '<li>Код товара: ' . $value['short_names'] . ', Остаток: ' . $value['quantity'] . 'шт;</li>';
+				} ?>
+		</ul>
+		<p>Итого для второго склада: <?php echo $sumQuantityWh(2);?> шт</p>
+	</div>
+	<div>
+
+		<?php 
+			$result = [];
+			$result[] = $shortNomenclatures1;
+			$result[] = $shortNomenclatures2;
+			echo $convertToJson($goods, $result);
+		?>
+	</div>
+</body>
+</html>
+
+
